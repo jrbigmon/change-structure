084b06ceea7ed8767708d24e4f7656c57fc3b6bf
jrbigmon <juniorterriaga0@gmail.com>
2023-01-20 08:51:22 -0300

    fix: removed execute function

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import {reorganizeStructure} from './reorganize-structure'
 
 module.exports = {
-  all: reorganizeStructure('controllers', 'services', 'repositories', 'entities')
+  all: reorganizeStructure
 }
